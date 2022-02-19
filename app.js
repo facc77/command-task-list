06c48b4b8d1d49eb23613ed7e9ece3ee43b5c250
facc77 <ferrerfacundo96@gmail.com>
2022-02-19 18:18:28 -0300

    create task and list task function added

---
+++ app.js
@@ -0,0 +1,40 @@
+require("colors");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const { guardarDB, leerDB } = require("./helpers/guardarArchivo");
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
+
+const main = async () => {
+  console.clear();
+
+  let opt = "";
+  const tareas = new Tareas();
+
+  const tareasDB = leerDB();
+
+  if (tareasDB) {
+    tareas.cargarTareasFromArray(tareasDB);
+  }
+
+  do {
+    //Imprimir menu
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case "1":
+        const desc = await leerInput("Descripción: ");
+        tareas.crearTarea(desc);
+        break;
+
+      case "2":
+        tareas.listadoCompleto();
+        break;
+    }
+
+    //guardarDB(tareas.listadoArr);
+
+    await pausa();
+  } while (opt !== "0");
+};
+
+main();
